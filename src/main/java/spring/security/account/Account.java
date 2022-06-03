ee65c60c5d2c2195007df82bda8b7ca1c7adf16d
ha-jinwoo <gkwlsdn95@naver.com>
2022-06-03 14:33:57 +0900

    스프링 시큐리티 커스터마이징: JPA 연동

---
+++ src/main/java/spring/security/account/Account.java
@@ -0,0 +1,30 @@
+package spring.security.account;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Account {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String role;
+
+    public void encodePassword() {
+        this.password = "{noop}" + this.getPassword();
+    }
+}
--- src/main/java/spring/security/config/SecurityConfig.java
+++ src/main/java/spring/security/config/SecurityConfig.java
@@ -1,33 +1,36 @@
 package spring.security.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import spring.security.account.AccountService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    AccountService accountService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .mvcMatchers("/", "/info", "/account/**", "/h2-console/**").permitAll()
                 .mvcMatchers("/", "/info").permitAll()
                 .mvcMatchers("/admin").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin()
-                .and()
-                .httpBasic()
-        ;
+                .anyRequest().authenticated();
+        http.formLogin();
+        http.httpBasic();
     }
-    // 인메모리 유저 추가
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("jinwoo").password("{noop}1234").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}1234!").roles("ADMIN");
-    }
+//    // 인메모리 유저 추가
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("jinwoo").password("{noop}1234").roles("USER")
+//                .and()
+//                .withUser("admin").password("{noop}1234!").roles("ADMIN");
+//    }
 }
