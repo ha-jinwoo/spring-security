84d9a1705ea635e12f012615f83cadbf10e1cbfc
ha-jinwoo <gkwlsdn95@naver.com>
2022-06-06 21:48:53 +0900

    스프링 시큐리티 - 스프링 시큐리티 테스트 1, 2부

--- src/main/java/spring/security/account/Account.java
+++ src/main/java/spring/security/account/Account.java
@@ -1,7 +1,6 @@
 package spring.security.account;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.persistence.Column;
@@ -12,6 +11,9 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Account {
 
     @Id
